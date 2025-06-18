c41b529068e3be0df41afa6fc2796c542e952541
Justinas Petkauskas <jplabsystems@gmail.com>
2025-06-18 19:57:52 -0400

    parsing start

--- main.java
+++ main.java
@@ -3,7 +3,6 @@ import java.io.*;
 
 public class main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
 
         File gcode = new File("/home/justinas/FRAIM/model.gcode");
         File pointCloud = new File("/home/justinas/FRAIM/pointcloudjava.txt");
@@ -11,9 +10,28 @@ public class main {
         try (BufferedReader reader = new BufferedReader(new FileReader(gcode))) {
 
             try (BufferedWriter writer = new BufferedWriter(new FileWriter(pointCloud))) {
+                
                 String line;
                 while ((line = reader.readLine()) != null) {
+                    
                     writer.write(line + "\n");
+                    System.out.println(line);
+
+                    /*
+                     * 
+                     */ 
+
+                    for(int i = 0; i < line.length() - 4; i++)
+                    {
+                        if(line.substring(i, i+1) == "G1"){
+                            writer.write(line + "\n");
+                        }
+                    }
+
+                     /* 
+                     * 
+                     */
+
                 }
             } catch (IOException e) {
                 e.printStackTrace();
